2ee903ad1138f5f88caebbbc76fceb3964862072
elviza <elvizahanify@gmail.com>
2023-01-25 00:05:13 -0800

    #25 #29 Protected questions endpoints

--- dnc/routers/questions.py
+++ dnc/routers/questions.py
@@ -1,30 +1,12 @@
-from fastapi import APIRouter, Response, Depends, Request, status
+from fastapi import APIRouter, Response, Depends
 from queries.questions import QuestionsRepository, QuestionOut, AnswersRepo
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-
-
-
-# @app.exception_handler(RequestValidationError)
-# async def validation_exception_handler(
-#     request: Request, exc: RequestValidationError
-# ):
-#     return JSONResponse(
-#         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-#         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
-#     )
+from authenticator import authenticator
 
 
 class Item(BaseModel):
     title: str
     size: int
-
-
-# @app.post("/items/")
-# async def create_item(item: Item):
-#     return item
 
 
 router = APIRouter()
@@ -35,6 +17,7 @@ def get_question(
     question_id: int,
     response: Response,
     repo: QuestionsRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> QuestionOut:
     return repo.get_one(question_id)
 
@@ -46,6 +29,7 @@ def check_answer(
     character_id: int = 0,
     char_answer: int = 0,
     repo: AnswersRepo = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ):
     if question_id == 0 or char_answer == 0 or character_id == 0:
         return {"detail": "Invalid query parameters."}
