d7722c09eeb8f575818f6535992a08d17092d244
Jackie Lee <jlee428@ucr.edu>
2023-01-25 00:11:42 -0800

    #17 protected endpoints

--- dnc/routers/maps.py
+++ dnc/routers/maps.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Depends
 from queries.maps import MapsRepo, MapsIn
+from authenticator import authenticator
 
 router = APIRouter()
 
 
 @router.get("/maps/{quest_id}")
-def get_maps(quest_id: int, repo: MapsRepo = Depends()):
+def get_maps(
+    quest_id: int,
+    repo: MapsRepo = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
     return repo.get(MapsIn(quest_id=quest_id))
